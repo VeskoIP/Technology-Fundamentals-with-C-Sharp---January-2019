1ace0ee8e2e65b7fc9ab4696f4dd6a32da2dcbf4
VeskoIP <veselin.vip@gmail.com>
2019-04-28 19:33:50 +0300

    Edited some projects

---
+++ 1.IntroAndBasicSyntax/6.StrongNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class StrongNumber
+{
+    static void Main()
+    {
+        string num = Console.ReadLine();
+        int numToInt = Int32.Parse(num);
+        int sum = 0;
+        int multiplication = 1;
+
+        for (int i = 0; i < num.Length; i++)
+        {
+            for (int k = 1; k <= (int)Char.GetNumericValue(num[i]); k++)
+            {
+                multiplication = multiplication * k;
+            }
+
+            sum += multiplication;
+            multiplication = 1;
+        }
+
+        if (sum == numToInt)
+        {
+            Console.WriteLine("yes");
+        }
+
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
